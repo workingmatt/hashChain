97dac4b2db74b94c465e450cf52a402f9c17e075
Matt Mapleston <matt@chanzachanzo.com>
2017-04-05 16:27:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(express.static(__dirname__+'/public'));
+app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
